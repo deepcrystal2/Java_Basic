78ce08b1f40a560c14ad74f337bdf72245f17126
deepcrystal2 <honestrusty0612@gmail.com>
2024-05-18 17:10:28 +0900

    HashMap & Iterator

---
+++ src/Chap_09/_08_Iterator.java
@@ -0,0 +1,92 @@
+package Chap_09;
+
+import java.util.*;
+
+public class _08_Iterator {
+    public static void main(String[] args) {
+        // 이터레이터
+        List<String> list = new ArrayList<>();
+        list.add("유재석");
+        list.add("(알수없음)");
+        list.add("김종국");
+        list.add("(알수없음)");
+        list.add("강호동");
+        list.add("(알수없음)");
+        list.add("박명수");
+        list.add("조세호");
+
+        for (String s : list) {
+            System.out.println(s);
+        }
+        System.out.println("==============");
+
+        Iterator<String> it = list.iterator();
+        System.out.println(it.next());
+        System.out.println(it.next());
+        System.out.println(it.next());
+        System.out.println(it.next());
+
+        System.out.println("==============");
+        it = list.iterator(); // 커서 처음 위치로 이동
+
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+        System.out.println("==============");
+
+        it = list.iterator();
+
+        while (it.hasNext()) {
+            String s = it.next();
+            if (s.contains("(알수없음)")) {
+                it.remove(); // 삭제
+            }
+        }
+
+        it = list.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+        System.out.println("=============");
+
+        HashSet<String> set = new HashSet<>();
+        set.add("유재석");
+        set.add("박명수");
+
+        Iterator<String> itSet = set.iterator();
+
+        while (itSet.hasNext()) {
+            System.out.println(itSet.next());
+        }
+
+        System.out.println("================");
+
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("유재석", 10);
+        map.put("박명수", 5);
+
+//        map.iterator() // 제공되지 않음
+        Iterator<String> itMapKey = map.keySet().iterator();
+
+        while (itMapKey.hasNext()) {
+            System.out.println(itMapKey.next());
+        }
+        System.out.println("===============");
+
+        Iterator<Integer> itValue = map.values().iterator();
+
+        while (itValue.hasNext()) {
+            System.out.println(itValue.next());
+        }
+
+        System.out.println("==============");
+
+        Iterator<Map.Entry<String, Integer>> itMap = map.entrySet().iterator();
+
+        while (itMap.hasNext()) {
+            System.out.println(itMap.next());
+
+        }
+    }
+}
