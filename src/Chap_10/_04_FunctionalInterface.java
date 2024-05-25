9e02d503070b728feebb4729b76709d2fd941d9a
deepcrystal2 <honestrusty0612@gmail.com>
2024-05-26 00:46:53 +0900

    Stream

--- src/Chap_10/_04_FunctionalInterface.java
+++ src/Chap_10/_04_FunctionalInterface.java
@@ -1,10 +1,20 @@
 package Chap_10;
 
+import Chap_10.converter.Convertable;
 import Chap_10.converter.KRWConverter;
 
 public class _04_FunctionalInterface {
     public static void main(String[] args) {
         KRWConverter converter = new KRWConverter();
-        converter.convert(1);
+//        converter.convert(2);
+
+//        convertUSD((USD) -> System.out.println(USD + "달러 = " + (USD * 1400) + "원"), 1);
+        Convertable convertable = (USD) -> System.out.println(USD + "달러 = " + (USD * 1400) + "원");
+        convertUSD(convertable, 1);
     }
+
+    public static void convertUSD(Convertable converter, int USD) {
+        converter.convert(USD);
+    }
+
 }
